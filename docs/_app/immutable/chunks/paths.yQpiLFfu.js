da5770ae172c369efebd6f179a998faff4ace290
Felipe Rolon <afra.9622@gmail.com>
2025-04-25 23:29:11 -0400

    Adding the first game DarkRunner to the games tab

---
+++ docs/_app/immutable/chunks/paths.yQpiLFfu.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1c5p1dk)==null?void 0:s.base)??"/arolon";var a;const t=((a=globalThis.__sveltekit_1c5p1dk)==null?void 0:a.assets)??e;export{t as a,e as b};
--- docs/_app/immutable/entry/start.CYpTiimk.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.B1g2iF4s.js";export{t as start};
