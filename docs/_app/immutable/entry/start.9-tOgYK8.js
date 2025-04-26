2920d88a21d275967982c756ab7fd21a8533bd87
Felipe Rolon <afra.9622@gmail.com>
2025-04-25 21:51:47 -0400

    Fix build

---
+++ docs/_app/immutable/entry/start.9-tOgYK8.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.C2XIDv2i.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
-import { defineConfig } from 'vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
@@ -17,10 +15,7 @@ const config = {
 		paths: {
 			base: '/arolon',
 		}
-	},
-	vite: defineConfig({
-		base: '/arolon/'
-	})
+	}
 };
 
 export default config;
